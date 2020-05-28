3743c9559a5f8951f1f9170fb47c0125634d9488
john beltrez <beltrezjohn@gmail.com>
2020-05-28 15:34:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," ",]
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+
 end 
 
 display_board
